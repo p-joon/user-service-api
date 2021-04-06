3676cc64d3e0fe303607b83cf0152aa5f92d31ec
Joon Won Park <jpark9493@gmail.com>
2021-04-06 17:14:57 -0400

    improve caching

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Express/Node API used for [this Docker tutorial](https://www.youtube.com/watch?v=Wf2eSG3owoA&ab_channel=freeCodeCamp.org)
--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.json({
-	name: 'test',
-	contact: 'test@test.test'
-}))
+app.get('/', (req, res) => res.json([
+	{
+		name: 'test1',
+		contact: 'test1@test.test'
+	},
+	{
+		name: 'test2',
+		contact: 'test2@test.test'
+	},
+	{
+		name: 'test3',
+		contact: 'test3@test.test'
+	}
+]))
 
 app.listen(port, () => {
 	  console.log(`Example app listening at http://localhost:${port}`)
